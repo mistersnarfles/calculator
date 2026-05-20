5a7905ee806ecaa1e11d4416715baca2f73ae5b4
mistersnarfles <devin.luhrsen@gmail.com>
2026-05-19 20:10:16 -0700

    added basic opeartor functions
    added function to call basic operators
    
    added variables for 2 numbers and an operator

---
+++ script.js
@@ -0,0 +1,25 @@
+let firstNumber
+let operator
+let secondNumber
+
+
+
+function add(a, b){
+    return a + b
+}
+
+function subtract(a , b){
+    return a - b
+}
+
+function multiply(a, b){
+    return a * b
+}
+
+function divide(a, b){
+    return a / b
+}
+
+function operate(operator, a, b){
+    return operator(a, b)
+}
