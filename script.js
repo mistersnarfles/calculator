826c594332b8fe991ef4a17ceb318c1b136d0b06
mistersnarfles <devin.luhrsen@gmail.com>
2026-05-21 20:13:04 -0700

    Added two features
    Added buttons that display when pressed
    
    Added clear button with function to clear display

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ let firstNumber
 let operator
 let secondNumber
 
+const buttonContainer = document.getElementById('button-container')
+const header = document.querySelector('h1')
+const clear = document.getElementById('clear')
+const display = document.getElementById('display')
+const results = document.getElementById('results')
+
+let numberList = []
 
 
 function add(a, b){
@@ -23,3 +30,19 @@ function divide(a, b){
 function operate(operator, a, b){
     return operator(a, b)
 }
+
+for (let i = 0; i < 10; i++){
+    const button = document.createElement('button')
+    numberList.push(button)
+    button.textContent = i
+    buttonContainer.appendChild(button)
+    button.addEventListener('click', () => {
+        display.textContent = i
+        header.appendChild(display)
+    })
+}
+
+clear.addEventListener('click', () => {
+    header.removeChild(display)
+})
+
